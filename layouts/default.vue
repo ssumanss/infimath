727d3573d83e98a9a62dcb0473d7094a331345eb
ssumanss <ssuman44@gmail.com>
2019-05-24 07:49:28 +0530

    nuxt bootstrap

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <nuxt/>
+  </div>
+</template>
+<script>
+  import Navbar from '../components/Navbar'
+  import Jumbotron from '../components/Jumbotron'
+  export default {
+    components: {
+      Navbar,
+      Jumbotron
+    }
+  }
+</script>
+<style>
+  html { font-family: Arial, sans-serif;}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <jumbotron></jumbotron>
+      <h1 class="title">Hi, I'm Allen Kim</h1>
+      <h2 class="subtitle">A software engineer in Canada.</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Jumbotron from '../components/Jumbotron'
+  export default {
+    components: {
+      Jumbotron
+    }
+  }
+</script>
